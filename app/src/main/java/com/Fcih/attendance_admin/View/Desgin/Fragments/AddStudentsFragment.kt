2a61cb7213fe0cedc9aeca58a6998567398d711d
Mohamed-Ashraf1 <mmidoaashraf@gmail.com>
2022-03-01 12:05:04 +0200

    Add-Students Fragment

---
+++ app/src/main/java/com/Fcih/attendance_admin/View/Desgin/Fragments/AddStudentsFragment.kt
@@ -0,0 +1,18 @@
+package com.Fcih.attendance_admin.View.Desgin.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.Fcih.attendance_admin.R
+
+
+class AddStudentsFragment : Fragment(R.layout.fragment_add_students) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
