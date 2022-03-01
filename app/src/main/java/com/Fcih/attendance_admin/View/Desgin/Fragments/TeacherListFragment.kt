c056a7a176d3c25be1c039a687320ff564869736
mohamedadel2323 <mohamedadel2323m@gmail.com>
2022-03-01 12:39:50 +0200

    Updated TeacherList Fragment design

--- app/src/main/java/com/Fcih/attendance_admin/View/Desgin/Fragments/TeacherListFragment.kt
+++ app/src/main/java/com/Fcih/attendance_admin/View/Desgin/Fragments/TeacherListFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.Fcih.attendance_admin.R
@@ -31,29 +33,39 @@ class TeacherListFragment : Fragment() {
 
         teacherListViewModel = ViewModelProvider(this).get(TeacherListViewModel::class.java)
 
-        val manager = GridLayoutManager(activity , 2)
+        val manager = GridLayoutManager(activity, 2)
 
         rv.layoutManager = manager
 
         var teachersList = mutableListOf(
-            Teacher("1","Amr Ghoniem"),
-            Teacher("2","Mai Eldafrawy"),
-            Teacher("3","Zlatan Ibrahimovic"),
-            Teacher("4","Cristiano Ronaldo"),
-            Teacher("1","حمثلاح"),
-            Teacher("2","Kilian Mbappe"),
-            Teacher("3","Lionel Messi"),
-            Teacher("4","Robert Lewandowski"),
-            Teacher("5","bla bla5"),
-            Teacher("6","bla bla6")
+            Teacher("1", "Amr Ghoniem"),
+            Teacher("2", "Mai Eldafrawy"),
+            Teacher("3", "Zlatan Ibrahimovic"),
+            Teacher("4", "Cristiano Ronaldo"),
+            Teacher("1", "حمثلاح"),
+            Teacher("2", "Kilian Mbappe"),
+            Teacher("3", "Lionel Messi"),
+            Teacher("4", "Robert Lewandowski"),
+            Teacher("5", "bla bla5"),
+            Teacher("6", "bla bla6")
         )
         var adapter = TeacherAdapter()
         adapter.submitList(teachersList)
         adapter.setOnItemClickListener {
-            Toast.makeText(application , it.teacherName, Toast.LENGTH_SHORT).show()
+//            Navigation.findNavController(this.requireView())
+//                .navigate()
+            Toast.makeText(application, it.teacherName, Toast.LENGTH_SHORT).show()
         }
 
         rv.adapter = adapter
+
+
+        var addTeacherTv = view?.findViewById<TextView>(R.id.mAddTeacherTv)
+        addTeacherTv?.setOnClickListener {
+//            Navigation.findNavController(this.requireView())
+//                .navigate()
+            Toast.makeText(application, "Clicked", Toast.LENGTH_SHORT).show()
+        }
 
         return view
     }
