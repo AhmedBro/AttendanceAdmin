16d794fb5c8d258e1ad0d505e5b2153d73588a59
Ahmed <Ahmed>
2022-03-04 00:02:19 +0200

    FireStore

--- app/src/main/java/com/Fcih/attendance_admin/View/Desgin/Fragments/AddStudentsFragment.kt
+++ app/src/main/java/com/Fcih/attendance_admin/View/Desgin/Fragments/AddStudentsFragment.kt
@@ -6,13 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.Fcih.attendance_admin.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class AddStudentsFragment : Fragment(R.layout.fragment_add_students) {
-
+    val db = Firebase.firestore
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
     }
+
 
 }
