a6aed70b6d3777b9dd5d3c63ad7e61980b6047ff
mohamedadel2323 <mohamedadel2323m@gmail.com>
2022-05-17 19:03:05 +0200

    Final

--- app/src/main/java/com/Fcih/attendance_admin/Desgin/Activities/MainActivity.kt
+++ app/src/main/java/com/Fcih/attendance_admin/Desgin/Activities/MainActivity.kt
@@ -3,6 +3,9 @@ package com.Fcih.attendance_admin.Desgin.Activities
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.Fcih.attendance_admin.R
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val window: Window = this.getWindow()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.green4))
 
     }
 
